43f891338604d7ba7c754cc81fcc6092f3b7f92d
Adelaj <kersenel@gmail.com>
2018-04-19 15:53:40 +0300

    ...

--- src/categorical.c
+++ src/categorical.c
@@ -1,10 +1,14 @@
+#include "np.h"
 #include "common.h"
+#include "ll.h"
 #include "categorical.h"
 
 #include <float.h>
+#include <math.h>
 #include <stdlib.h>
+#include <gsl/gsl_cdf.h>
+#include <gsl/gsl_sf.h>
 #include <gsl/gsl_randist.h>
-#include <gsl/gsl_sf_gamma.h>
 
 #define REMAP(remap, cd) \
     switch ((cd)) \
@@ -38,20 +42,26 @@
         continue; \
     }
 
-bool moving_average_adjusted(uint8_t *gen, uint8_t *phen, size_t snp_cnt, size_t phen_cnt, size_t rmax, size_t k)
+enum maver_adj_status {
+    MOVING_AVERAGE_STATUS_ERROR = 0,
+    MOVING_AVERAGE_STATUS_SUCCESS,
+    MOVING_AVERAGE_STATUS_SATURATION
+};
+
+enum maver_adj_status maver_adj(uint8_t *gen, uint8_t *phen, size_t snp_cnt, size_t phen_cnt, size_t rpl_max, size_t k, double rel_err, double *res)
 {
     //SEXP res, dim = getAttrib(genotype, R_DimSymbol);
     //const unsigned phen_cnt = INTEGER(dim)[0], snp_cnt = INTEGER(dim)[1];
-    const double relError = 1. + 1.e-7;
+    //const double relError = 1. + 1.e-7;
 
     //PROTECT(res = allocVector(REALSXP, 1));
 
-    unsigned table[9], sumy[3], remapy[3];
+    size_t table[9], sumy[3], remapy[3];
     double outer[9];
 
-    unsigned *permut = NULL;
-    unsigned *row_count = NULL, *row_tablesum = NULL, *row_filter = NULL, *row_sums = NULL;
-    unsigned char *row_unique = NULL, *row_remap = NULL;
+    size_t *permut = NULL;
+    size_t *row_count = NULL, *row_tablesum = NULL, *row_filter = NULL, *row_sums = NULL;
+    uint8_t *row_unique = NULL, *row_remap = NULL;
         
     // Memory management
     permut = malloc((size_t) phen_cnt * sizeof *permut);
@@ -62,7 +72,7 @@ bool moving_average_adjusted(uint8_t *gen, uint8_t *phen, size_t snp_cnt, size_t
     row_unique = calloc((size_t) snp_cnt, sizeof *row_unique); // Filled with zeros!
     row_remap = malloc(3 * (size_t) snp_cnt * sizeof *row_remap);
 
-    if (!(permut && row_count && row_tablesum && row_filter && row_sums && row_unique && row_remap)) goto error1; 
+    if (!(permut && row_count && row_tablesum && row_filter && row_sums && row_unique && row_remap)) goto error;
     // Memory allocation failed
 
     ///////////////////////////////////////////////////////////////////////////
@@ -75,44 +85,44 @@ bool moving_average_adjusted(uint8_t *gen, uint8_t *phen, size_t snp_cnt, size_t
 
     for (unsigned j = 0; j < snp_cnt; j++)
     {
-        const size_t phen_cntj = (size_t) phen_cnt * (size_t) j;
+        const size_t phen_off = phen_cnt * j;
 
         // Filtering genotypes
-        unsigned selcount = 0;
-        for (unsigned i = 0; i < phen_cnt; i++) if (INTEGER(genotype)[i + phen_cntj] != NA_INTEGER && INTEGER(genotype)[i + phen_cntj] != 3) row_filter[selcount++ + phen_cntj] = i;
+        size_t sel_cnt = 0;
+        for (size_t i = 0; i < phen_cnt; i++) if (gen[i + phen_off] < 3) row_filter[sel_cnt++ + phen_off] = i;
 
-        if (!selcount) continue; // There is nothing to do if the count is zero
-        row_count[j] = selcount;
+        if (!sel_cnt) continue; // There is nothing to do if the count is zero
+        row_count[j] = sel_cnt;
 
         // Working with rows
-        unsigned char cdx = 0;
-        unsigned char *const remapx = row_remap + 3 * j;
-        for (unsigned i = 0; i < selcount && cdx != 7; cdx |= 1 << INTEGER(genotype)[row_filter[i++ + phen_cntj] + phen_cntj]);
+        uint8_t cdx = 0;
+        uint8_t *remapx = row_remap + 3 * j;
+        for (size_t i = 0; i < sel_cnt && cdx != 7; cdx |= 1 << gen[row_filter[i++ + phen_off] + phen_off]);
         REMAP(remapx, cdx); // Remapping table row indeces
         row_unique[j] = cdx;
 
         // Working with columns
-        unsigned char cdy = 0;
-        for (unsigned i = 0; i < selcount && cdy != 7; cdy |= 1 << INTEGER(phenotype)[row_filter[i++ + phen_cntj]]);
+        uint8_t cdy = 0;
+        for (size_t i = 0; i < sel_cnt && cdy != 7; cdy |= 1 << phen[row_filter[i++ + phen_off]]);
         REMAP(remapy, cdy); // Remapping table column indeces
 
-                            // Building contingency table
-        memset(table, 0, cdx * cdy * sizeof table[0]);
+        // Building contingency table
+        memset(table, 0, cdx * cdy * sizeof(*table));
 
-        for (unsigned i = 0; i < selcount; i++)
+        for (size_t i = 0; i < sel_cnt; i++)
         {
-            unsigned ind = row_filter[i + phen_cntj];
-            table[remapx[INTEGER(genotype)[ind + phen_cntj]] + cdx * remapy[INTEGER(phenotype)[ind]]]++;
+            size_t ind = row_filter[i + phen_off];
+            table[remapx[gen[ind + phen_off]] + cdx * remapy[phen[ind]]]++;
         }
 
         // Computing sums
         unsigned *const sumx = row_sums + 3 * j;
 
-        memset(sumx, 0, cdx * sizeof sumx[0]);
-        memset(sumy, 0, cdy * sizeof sumy[0]);
+        memset(sumx, 0, cdx * sizeof(*sumx));
+        memset(sumy, 0, cdy * sizeof(*sumy));
         row_tablesum[j] = 0;
 
-        for (unsigned char t = 0; t < cdy; row_tablesum[j] += sumy[t], t++) for (unsigned char s = 0; s < cdx; s++)
+        for (uint8_t t = 0; t < cdy; row_tablesum[j] += sumy[t], t++) for (uint8_t s = 0; s < cdx; s++)
         {
             unsigned el = table[s + cdx * t];
             sumx[s] += el;
@@ -120,46 +130,45 @@ bool moving_average_adjusted(uint8_t *gen, uint8_t *phen, size_t snp_cnt, size_t
         }
 
         // Computing outer product
-        double outer_min = DBL_MAX;
+        bool exact = 0;
 
-        for (unsigned char t = 0; t < cdy; t++) for (unsigned char s = 0; s < cdx; s++)
+        for (uint8_t t = 0; t < cdy; t++) for (uint8_t s = 0; s < cdx; s++)
         {
-            unsigned char ind = s + cdx * t;
-            outer[ind] = (double) sumx[s] * (double) sumy[t] / (double) row_tablesum[j];
-            if (outer[ind] < outer_min) outer_min = outer[ind];
+            uint8_t ind = s + cdx * t;
+            double tmp = outer[ind] = (double) sumx[s] * (double) sumy[t] / (double) row_tablesum[j];
+            if (tmp >= 5. || cdx > 2 || cdy > 2) continue;
+            exact = 1;
+            break;
         }
 
-        // Computing p-values
-        if (outer_min <= 4. && cdx == 2 && cdy == 2)
+        if (exact) // Exact Fisher test
         {
-            const unsigned lo = max(0, (signed) sumx[0] - (signed) sumy[1]);
-            const unsigned hi = min(sumx[0], sumy[0]);
+            const size_t lo = size_sub_sat(sumx[0], sumy[1]);
+            const size_t hi = MIN(sumx[0], sumy[0]);
 
-            const double hyp_comp = Rf_dhyper(table[0], sumy[0], sumy[1], sumx[0], 0);
+            const double hyp_comp = gsl_ran_hypergeometric_pdf(table[0], sumy[0], sumy[1], sumx[0]);
             double hyp_sum = 0., hyp_less = 0.;
 
-            for (unsigned ind = lo; ind <= hi; ind++)
+            for (size_t ind = lo; ind <= hi; ind++)
             {
-                double hyp = Rf_dhyper(ind, sumy[0], sumy[1], sumx[0], 0);
+                double hyp = gsl_ran_hypergeometric_pdf(ind, sumy[0], sumy[1], sumx[0]);
                 hyp_sum += hyp;
-                if (hyp <= hyp_comp * relError) hyp_less += hyp;
+                if (hyp <= hyp_comp * rel_err) hyp_less += hyp;
             }
 
-            density += log(hyp_sum) - log(hyp_less);
+            density += log10(hyp_sum) - log10(hyp_less);
         }
-        else
+        else // Chi-square test
         {
             double stat = 0.;
-
-            for (unsigned char ind = 0; ind < cdx * cdy; ind++)
+            for (uint8_t ind = 0; ind < cdx * cdy; ind++)
             {
                 double diff = outer[ind] - (double) table[ind];
                 stat += diff * diff / outer[ind];
             }
 
-            density -= Rf_pchisq(stat, (cdx - 1) * (cdy - 1), 0, 1); // Upper tail and logarithm are enabled
+            density -= log10(gsl_cdf_chisq_Q(stat, (cdx - 1) * (cdy - 1)));
         }
-
         density_count++;
     }
 
@@ -170,12 +179,9 @@ bool moving_average_adjusted(uint8_t *gen, uint8_t *phen, size_t snp_cnt, size_t
     //  Simulations
     //
 
-    const size_t rbnd = (size_t) REAL(rmax)[0], qcbnd = INTEGER(k)[0];
-    const _Bool fixed = qcbnd == NA_INTEGER; // Entering 'fixed' mode if 'k' is 'NA'
-
     size_t rpl = 0, qc = 0;
 
-    for (; rpl < rbnd && (fixed || qc <= qcbnd); rpl++)
+    for (; rpl < rpl_max && (!k || qc <= k); rpl++) // Entering 'fixed' mode if 'k' is 'NA'
     {
         // Generating random permutation
         for (unsigned i = 0; i < phen_cnt; permut[i] = i, i++); // Starting with identity permutation
@@ -195,35 +201,35 @@ bool moving_average_adjusted(uint8_t *gen, uint8_t *phen, size_t snp_cnt, size_t
         {
             const size_t phen_cntj = (size_t) phen_cnt * (size_t) j;
             const unsigned selcount = row_count[j];
-            const unsigned char cdx = row_unique[j];
-            unsigned char *const remapx = row_remap + 3 * j;
+            const uint8_t cdx = row_unique[j];
+            uint8_t *const remapx = row_remap + 3 * j;
 
             // Working with columns
-            unsigned char cdy = 0;
-            for (unsigned i = 0; i < selcount && cdy != 7; cdy |= 1 << INTEGER(phenotype)[permut[row_filter[i++ + phen_cntj]]]);
+            uint8_t cdy = 0;
+            for (unsigned i = 0; i < selcount && cdy != 7; cdy |= 1 << phen[permut[row_filter[i++ + phen_cntj]]]);
             REMAP(remapy, cdy); // Remapping table column indeces
 
-                                // Building contingency table
+            // Building contingency table
             memset(table, 0, cdx * cdy * sizeof table[0]);
 
             for (unsigned i = 0; i < selcount; i++)
             {
                 unsigned ind = row_filter[i + phen_cntj];
-                table[remapx[INTEGER(genotype)[ind + phen_cntj]] + cdx * remapy[INTEGER(phenotype)[permut[ind]]]]++;
+                table[remapx[gen[ind + phen_cntj]] + cdx * remapy[phen[permut[ind]]]]++;
             }
 
             // Computing sums
             unsigned *const sumx = row_sums + 3 * j; // Row sums are already computed
 
             memset(sumy, 0, cdy * sizeof sumy[0]);
-            for (unsigned char t = 0; t < cdy; t++) for (unsigned char s = 0; s < cdx; sumy[t] += table[s + cdx * t], s++);
+            for (uint8_t t = 0; t < cdy; t++) for (uint8_t s = 0; s < cdx; sumy[t] += table[s + cdx * t], s++);
 
             // Computing outer product
             double outer_min = DBL_MAX;
 
-            for (unsigned char t = 0; t < cdy; t++) for (unsigned char s = 0; s < cdx; s++)
+            for (uint8_t t = 0; t < cdy; t++) for (uint8_t s = 0; s < cdx; s++)
             {
-                unsigned char ind = s + cdx * t;
+                uint8_t ind = s + cdx * t;
                 outer[ind] = (double) sumx[s] * (double) sumy[t] / (double) row_tablesum[j];
                 if (outer[ind] < outer_min) outer_min = outer[ind];
             }
@@ -234,14 +240,14 @@ bool moving_average_adjusted(uint8_t *gen, uint8_t *phen, size_t snp_cnt, size_t
                 const unsigned lo = max(0, (signed) sumx[0] - (signed) sumy[1]);
                 const unsigned hi = min(sumx[0], sumy[0]);
 
-                const double hyp_comp = Rf_dhyper(table[0], sumy[0], sumy[1], sumx[0], 0);
+                const double hyp_comp = gsl_ran_hypergeometric_pdf(table[0], sumy[0], sumy[1], sumx[0]);
                 double hyp_sum = 0., hyp_less = 0.;
 
                 for (unsigned ind = lo; ind <= hi; ind++)
                 {
                     double hyp = Rf_dhyper(ind, sumy[0], sumy[1], sumx[0], 0);
                     hyp_sum += hyp;
-                    if (hyp <= hyp_comp * relError) hyp_less += hyp;
+                    if (hyp <= hyp_comp * rel_err) hyp_less += hyp;
                 }
 
                 density_ += log(hyp_sum) - log(hyp_less);
@@ -250,7 +256,7 @@ bool moving_average_adjusted(uint8_t *gen, uint8_t *phen, size_t snp_cnt, size_t
             {
                 double stat = 0.;
 
-                for (unsigned char ind = 0; ind < cdx * cdy; ind++)
+                for (uint8_t ind = 0; ind < cdx * cdy; ind++)
                 {
                     double diff = outer[ind] - (double) table[ind];
                     stat += diff * diff / outer[ind];
@@ -266,8 +272,18 @@ bool moving_average_adjusted(uint8_t *gen, uint8_t *phen, size_t snp_cnt, size_t
         if (density_ > density) qc++;
     }
 
-    REAL(res)[0] = (double) qc / (double) rpl;
-    if (!fixed && rpl == rbnd) REAL(res)[0] *= -1;
+   
+
+    *res = (double) qc / (double) rpl;
+    if  (k && rpl == rpl_max) res *= -1;
+
+    // Error handling
+    for (;;)
+    {
+    error1:
+        //Rf_warning("Function '%s' exited with an error: memory allocation failed!\n", __FUNCTION__);
+        break;
+    }
 
 epilogue:
     // Memory should be cleared after all...
@@ -280,23 +296,7 @@ epilogue:
     free(row_unique);
     free(row_remap);
 
-    UNPROTECT(1);
     return res;
-
-    // Error handling
-    for (;;)
-    {
-    error0:
-        Rf_warning("Function '%s' exited with an error: dimensions of genotype and phenotype are discordant!\n", __FUNCTION__);
-        break;
-
-    error1:
-        Rf_warning("Function '%s' exited with an error: memory allocation failed!\n", __FUNCTION__);
-        break;
-    }
-
-    REAL(res)[0] = NA_REAL;
-    goto epilogue;
 }
 
 p_test()
