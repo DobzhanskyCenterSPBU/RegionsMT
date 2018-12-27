52a3de0dba680ca78a8ec8c63d792bb49deefb2f
Adelaj <kersenel@gmail.com>
2018-12-27 17:15:18 +0300

    Update MySQL-Dispatch.c

--- Source/MySQL-Dispatch.c
+++ Source/MySQL-Dispatch.c
@@ -69,7 +69,7 @@ DEFINE_WRITEPROC(Alias, "\"%.31s\"", res->snpnamestr + res->snpname[_index])
 //DEFINE_WRITEPROC(Allele, "\"%.31s\"", res->allelenamestr + res->allelename[_index])
 
 DEFINE_WRITEPROC_V(Lpv, "%.16e", isnan((res->nlpv)[_index]) ? -1. : pow(10, -res->nlpv[_index]))
-DEFINE_WRITEPROC_V(Qas, "%.16e", res->qas[_index])
+DEFINE_WRITEPROC_V(Qas, "%.16e", isfinite(res->qas[_index]) ? res->qas[_index] : HUGE_VAL)
 //DEFINE_WRITEPROC_V(Maf, "%.16e", isnan((res->maf)[_index]) ? -1. : res->maf[_index])
 DEFINE_WRITEPROC_V(Rlpv, "%" PRIuPTR, res->rnlpv[_index] + 1)
 DEFINE_WRITEPROC_V(Rqas, "%" PRIuPTR, res->rqas[_index] + 1)
